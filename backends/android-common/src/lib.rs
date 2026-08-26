04ca47007ec1df85262e2905323ddc11a7d0aa4f
cavivie <cavivie@gmail.com>
2026-08-26 12:34:08 +0800

    refactor(android): keep backends self-contained

--- backends/android-common/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Shared implementation details for Nyquest backends using Android Java HTTP APIs.
-
-#![cfg_attr(docsrs, feature(doc_cfg))]
-
-#[cfg(target_os = "android")]
-mod android;
-
-#[cfg(target_os = "android")]
-pub use android::{AndroidAsyncClient, AndroidBackend, AndroidBlockingClient, BackendBindings};
--- backends/android-httpengine/src/lib.rs
+++ backends/android-httpengine/src/lib.rs
@@ -6,9 +6,12 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 #[cfg(target_os = "android")]
-use jni::{objects::JObject, JNIEnv};
+mod android;
+
 #[cfg(target_os = "android")]
-use nyquest_backend_android_common::{AndroidBackend, BackendBindings};
+use android::{AndroidBackend, BackendBindings};
+#[cfg(target_os = "android")]
+use jni::{objects::JObject, JNIEnv};
 
 /// The backend implementation using an application-provided `android.net.http.HttpEngine`.
 #[derive(Clone)]
@@ -42,7 +45,7 @@ pub fn register(backend: HttpEngineBackend) {
 
 #[cfg(all(target_os = "android", feature = "async"))]
 impl nyquest_interface::r#async::AsyncBackend for HttpEngineBackend {
-    type AsyncClient = nyquest_backend_android_common::AndroidAsyncClient;
+    type AsyncClient = android::AndroidAsyncClient;
 
     async fn create_async_client(
         &self,
@@ -54,7 +57,7 @@ impl nyquest_interface::r#async::AsyncBackend for HttpEngineBackend {
 
 #[cfg(all(target_os = "android", feature = "blocking"))]
 impl nyquest_interface::blocking::BlockingBackend for HttpEngineBackend {
-    type BlockingClient = nyquest_backend_android_common::AndroidBlockingClient;
+    type BlockingClient = android::AndroidBlockingClient;
 
     fn create_blocking_client(
         &self,
